7ffcf89978ebeffe47347cc8b05e7991921d6ebc
“webee120” <mwwwg@naver.com>
2025-08-13 18:42:08 +0900

    123451231

--- js/main.js
+++ js/main.js
@@ -4,18 +4,42 @@
       $(".main .main-device .after-sethub").hide()
       $(".main .main-device .index-normal").hide()
 
+      // 허브등록 index가 보임
       if (document.referrer.includes("/mypage/device/management.html")) {
         $(".main .main-device .from-login").show().siblings().hide();
       }
+
+      // 기기등록 index가 보임
       if (document.referrer.includes("/mypage/device/install.html")) {
         $(".main .main-device .after-sethub").show().siblings().hide();
       }
       if (document.referrer.includes("/mypage/device/addevice.html")) {
         $(".main .main-device .after-sethub").show().siblings().hide();
       }
+
+      // 일반 index가 보임
       if (document.referrer.includes("/mypage/device/addevice2.html")) {
         $(".main .main-device .index-normal").show().siblings().hide();
       }
+      if (document.referrer.includes("/sensor/frontdoor.html")) {
+        $(".main .main-device .index-normal").show().siblings().hide();
+      }
+      if (document.referrer.includes("/sensor/refrigerator.html")) {
+        $(".main .main-device .index-normal").show().siblings().hide();
+      }
+      if (document.referrer.includes("/sensor/bedroom.html")) {
+        $(".main .main-device .index-normal").show().siblings().hide();
+      }
+      if (document.referrer.includes("/sensor/bathroom.html")) {
+        $(".main .main-device .index-normal").show().siblings().hide();
+      }
+      if (document.referrer.includes("/sensor/emergencybell.html")) {
+        $(".main .main-device .index-normal").show().siblings().hide();
+      }
+      if (document.referrer.includes("/alert/main.html")) {
+        $(".main .main-device .index-normal").show().siblings().hide();
+      }
+
 
 
 
