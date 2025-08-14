35cda011e7da283370714f176093ec0f0f320e14
HyeRan-Lee1 <hr94488@gmail.com>
2025-08-14 13:20:59 +0900

    리포트달력

--- report/report.js
+++ report/report.js
@@ -7,22 +7,45 @@ $("document").ready(function(){
     $(".list > li").eq($(this).index()).show().siblings().hide();
   })
 
+  //달력
+  $('.day .calendar').click(function(){
+      $("#dayModal").addClass('modal-open');
+  });
+
+  $('#dayModal .modal-close').click(function(){
+      $("#dayModal").removeClass('modal-open');
+  });
+
+  $(".day .calendar").click(function(){
+    $("#dayModal .calendar-modal-body").scrollTop(320);
+  });
+
+   //달력
+  $('.week .calendar').click(function(){
+      $("#weekModal").addClass('modal-open');
+  });
+
+  $('.modal-close').click(function(){
+      $("#weekModal").removeClass('modal-open');
+  });
+
+  $(".week .calendar").click(function(){
+    $(".calendar-modal-body").scrollTop(320);
+  });
+
+   //달력
+  $('.month .calendar').click(function(){
+      $("#monthModal").addClass('modal-open');
+  });
+
+  $('.modal-close').click(function(){
+      $("#monthModal").removeClass('modal-open');
+  });
+
+  $(".month .calendar").click(function(){
+    $(".calendar-modal-body").scrollTop(320);
+  });
+
 });
 
 
-function laypopId(id){
-
-        var $pop = $(id);
-
-        $('.calendar').click(function(){
-            $pop.addClass('modal-open');
-        });
-
-        $('.modal-close').click(function(){
-            $pop.removeClass('modal-open');
-        });
-
-        $(".calendar").click(function(){
-          $(".calendar-modal-body").scrollTop(380);
-        });
-      }
