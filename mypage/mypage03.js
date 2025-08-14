bd0fe5e09f364506a442ccc8b12031361357798f
HyeRan-Lee1 <hr94488@gmail.com>
2025-08-14 14:30:03 +0900

    탈퇴

--- mypage/mypage03.js
+++ mypage/mypage03.js
@@ -27,17 +27,4 @@ document.addEventListener("DOMContentLoaded", function() {
   
 })
 
-document.addEventListener('DOMContentLoaded', function () {
-  const unsubscribeModal = document.querySelector('.modal.unsubscribe');
-  const quitModal = document.querySelector('.modal.quit');
-  const unsubscribeBtn = document.querySelector('.modal.unsubscribe .button');
 
-
-  quitModal.style.display = 'none';
-
-  unsubscribeBtn.addEventListener('click', function (e) {
-    e.preventDefault(); 
-    unsubscribeModal.style.display = 'none';
-    quitModal.style.display = 'block'; 
-  });
-});
